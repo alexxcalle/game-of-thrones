3b9fd416eb69a793f23d0dc2c8f38463dc1d4901
alexxcalle <axcalle@sudamericano.edu.ec>
2024-06-10 19:02:49 -0500

    inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  {
+    path: 'welcome',
+    loadChildren: () =>
+      import('./pages/welcome/welcome.routes').then((m) => m.WELCOME_ROUTES),
+  },
+];
+
+NgModule({ imports: [Router, HttpClient], exports: [Router] });
